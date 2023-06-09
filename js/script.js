a7cc8676ef2c804a27b5e71674ae5029bba0eb6f
andreacassini <andrea1316@hotmail.it>
2023-06-09 18:54:00 +0200

    Add array of obj

--- js/script.js
+++ js/script.js
@@ -3,10 +3,27 @@ const {createApp} = Vue;
 createApp ({
     data(){
         return{
-
+            tasks : [
+                {
+                    text: 'Fare lavatrice',
+                    done: true
+                },
+                {
+                    text: 'Impostare timer',
+                    done: false
+                },
+                {
+                    text: 'Fare spesa',
+                    done: true
+                },
+                {
+                    text: 'Comprare latte',
+                    done: false
+                }
+            ]
         } 
     },
-    
+
     methods: {
             
         }
